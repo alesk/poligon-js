796ccce7aa0eaef349e0ef1d62d1bec3519f155c
Aleš Kotnik <ales.kotnik@gmail.com>
2014-03-07 10:09:22 +0100

    Add common.js to be a place for shared functionality.

---
+++ js/common.js
@@ -0,0 +1,35 @@
+/* jshint -W054 */ 
+
+// Template compiler for components supporting customisable templates
+// taken from JavaScript Ninja Manual, p. 248, modified a bit.
+// Use `<% ... %>` to execute blocks of JavaScript, `<%= ... %>` to write
+// out the result of the embedded expression.
+function tmpl(str, data) {
+    var fn = str.indexOf('<%') < 0 ?
+
+    // if there is no <% tags just return string
+    function () { return str; } :
+
+    // Generate a reusable function that will serve as a template generator.
+    new Function("o",
+        // Introduce the data as local variables using with(){}
+        "var p=[];with(o){p.push('" +
+
+            // Convert the template into pure JavaScript
+            str
+                .replace(/[\r\t\n]/g, " ")
+                .split("<%").join("\t")
+                .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                .replace(/\t=(.*?)%>/g, "',$1,'")
+                .split("\t").join("');")
+                .split("%>").join("p.push('")
+                .split("\r").join("\\'") +
+
+            "');}return p.join('');"
+    );
+
+    // Provide some basic currying to the user
+    return data ? fn(data) : fn;
+}
+
+
